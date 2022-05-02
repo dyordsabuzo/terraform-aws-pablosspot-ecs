02c5efa81274db44d7ad21a44c11ff03bbe57b05
George Abuzo <dyordsabuzo@gmail.com>
2022-05-02 14:58:00 +1000

    Set logconfiguration using cloudwatch log resource

--- locals.tf
+++ locals.tf
@@ -3,4 +3,17 @@ locals {
   first_container      = local.container_definition[0]
   main_container_name  = local.first_container.name
   main_container_port  = local.first_container.portMappings[0].containerPort
+
+  enhanced_container_definition = [
+    for definition in local.container_definition :
+    merge(definition, lookup(definition, "logConfiguration", null) ? {
+      logDriver = "awslogs"
+
+      options = {
+        awslogs-region        = var.region
+        awslogs-stream-prefix = definition.name
+        awslogs-group         = aws_cloudwatch_log_group.log.name
+      }
+    } : {})
+  ]
 }
