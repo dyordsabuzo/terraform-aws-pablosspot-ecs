1a3bb7132380bfba12065c64348855ac32d9d7c6
George Abuzo <dyordsabuzo@gmail.com>
2022-05-02 16:48:35 +1000

    Fix log configuration block

--- locals.tf
+++ locals.tf
@@ -1,18 +1,20 @@
 locals {
-  container_definition = jsondecode(var.container_definition)
+  container_definition = var.container_definition
   first_container      = local.container_definition[0]
   main_container_name  = local.first_container.name
   main_container_port  = local.first_container.portMappings[0].containerPort
 
   enhanced_container_definition = [
     for definition in local.container_definition :
-    merge(definition, lookup(definition, "logConfiguration", null) == null ? {
-      logDriver = "awslogs"
+    merge(definition, lookup(definition, "logConfiguration", null) ? {
+      logConfiguration = {
+        logDriver = "awslogs"
 
-      options = {
-        awslogs-region        = var.region
-        awslogs-stream-prefix = definition.name
-        awslogs-group         = aws_cloudwatch_log_group.log.name
+        options = {
+          awslogs-region        = var.region
+          awslogs-stream-prefix = definition.name
+          awslogs-group         = aws_cloudwatch_log_group.log.name
+        }
       }
     } : null)
   ]
