3232f9d48a19d1b143019c4a92f6567c420ee66f
dyordsabuzo <dyordsabuzo@gmail.com>
2022-05-02 14:59:40 +1000

    Set logconfiguration using cloudwatch log resource (#1)

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_ecs_task_definition" "task" {
   network_mode             = var.network_mode
   requires_compatibilities = ["EC2"]
   execution_role_arn       = aws_iam_role.task_role.arn
-  container_definitions    = var.container_definition
+  container_definitions    = jsonencode(local.enhanced_container_definition)
 }
 
 resource "aws_iam_role" "task_role" {
